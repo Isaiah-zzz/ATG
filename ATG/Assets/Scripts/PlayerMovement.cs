882e5e91bf88ec8a05eaf9c5694f1c857233dd79
Andrew <android031894@gmail.com>
2024-10-31 05:30:00 -0500

    mouse catapult control working on y axis

--- ATG/Assets/Scripts/PlayerMovement.cs
+++ ATG/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -8,10 +9,10 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
     [SerializeField] private float longJumpPower;
-    [SerializeField] private float playerScale;
     [SerializeField] private float grav;
     [SerializeField] private float gravMultiplier;
     [SerializeField] private float maxFallSpeed = 26f;
+    [SerializeField] private float friction;
 
     // coyote time and jump buffer variables
     [SerializeField] private float coyoteTime;
@@ -24,27 +25,33 @@ public class NewBehaviourScript : MonoBehaviour
     private float xMomentum = 0;
 
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float dashSpeed;
     [SerializeField] private TrailRenderer tr;
     private BoxCollider2D boxCollider;
+    private float playerScale;
     private Rigidbody2D body;
 
     // cameras being used in test scene
     [SerializeField] private GameObject cam1;
     [SerializeField] private GameObject cam2;
 
+    LineRenderer lr;
+    Vector2 DragStartPos;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
         boxCollider = GetComponent<BoxCollider2D>();
+        playerScale = transform.localScale.x;
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        // Vector3 mousePos = Input.mousePosition;
 
         // flip player direction depending on movement
-        if(horizontalInput > 0.01f){
+        if (horizontalInput > 0.01f){
             transform.localScale = new Vector3(playerScale, playerScale, playerScale);
         } 
         else if (horizontalInput < -0.01f)
@@ -115,26 +122,56 @@ public class NewBehaviourScript : MonoBehaviour
             longJumpReady = false;
         }
 
-        // long jump functionality
-        if (IsGrounded() && longJumpReady && jumpBufferCounter > 0f)
+        if (Input.GetMouseButton(0))
         {
-            // TODO: tidy this up, 3 input fields into Longjump?
-            if(Input.GetKey(KeyCode.A))
-            {
-                LongJump(0);
-            }
-            else if(Input.GetKey(KeyCode.W))
-            {
-                LongJump(1);
-            }
-            else if(Input.GetKey(KeyCode.D))
-            {
-                LongJump(2);
-            }
+
+        }
+
+        if(IsGrounded() && longJumpReady && jumpBufferCounter > 0f)
+        {
+            DragStartPos = Camera.main.ScreenToWorldPoint(body.position);
+            Vector2 DragEndPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Vector2 _velocity = (DragEndPos - DragStartPos) * longJumpPower;
+            float xPow = (DragEndPos.x - DragStartPos.x) * longJumpPower;
+            float yPow = Mathf.Min(13, DragEndPos.y - DragStartPos.y) * longJumpPower;
+            Vector2 _velocity = new(xPow, yPow);
+            print("x: "+ (DragEndPos.x - DragStartPos.x));
+            print("y: "+ (DragEndPos.y - DragStartPos.y));
+            print("Start: " + (DragStartPos));
+            print("End: " + (DragEndPos));
+            longJumpReady = false;
+
+            body.velocity = _velocity;
             jumpBufferCounter = 0f;
         }
+
+        // long jump functionality
+        // if (IsGrounded() && longJumpReady && jumpBufferCounter > 0f)
+        // {
+        //     // TODO: tidy this up, 3 input fields into Longjump?
+        //     if(Input.GetKey(KeyCode.A))
+        //     {
+        //         LongJump(0);
+        //     }
+        //     else if(Input.GetKey(KeyCode.W))
+        //     {
+        //         LongJump(1);
+        //     }
+        //     else if(Input.GetKey(KeyCode.D))
+        //     {
+        //         LongJump(2);
+        //     }
+        //     jumpBufferCounter = 0f;
+        // }
+        // // normal jump functionality
+        // else if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
+        // {
+        //     Jump();
+        //     jumpBufferCounter = 0f;
+        // }
+
         // normal jump functionality
-        else if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
         {
             Jump();
             jumpBufferCounter = 0f;
