7d40a01e45b582a4ed8ad2acad52bf9ea0242506
Loden Campbell <loden.campbell@gmail.com>
2024-11-26 12:37:06 -0600

    Added Collectibles, started Intro UI

---
+++ ATG/Assets/Scripts/Player/CollectibleScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleScript : MonoBehaviour
+{
+    public float respawnTime = 1.0f;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collide;
+    [SerializeField] private PlayerTossGrowth playerTossGrowth;
+    private int collectCount;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collide = GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collect();
+            playerTossGrowth.AddCount(this.gameObject.tag);
+        }
+    }
+
+    void Collect()
+    {
+        spriteRenderer.enabled = false;
+        collide.enabled = false;
+        StartCoroutine(RespawnAfterDelay());
+    }
+
+    IEnumerator RespawnAfterDelay()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        spriteRenderer.enabled = true;
+        collide.enabled = true;
+    }
+}
