858e4c22101bec74872a01e7e059e942c61ff558
caseyst22 <ccstrayer02@gmail.com>
2024-11-27 16:24:11 -0600

    Enemy turns around on ledge, does not turn around on wall

--- ATG/Assets/Scripts/EnemyScript.cs
+++ ATG/Assets/Scripts/EnemyScript.cs
@@ -12,21 +12,31 @@ public class EnemyScript : MonoBehaviour
     // toggleable bool for enemy to chase player on x axis
     [SerializeField] bool targetsPlayer = false;
 
+    // Player object to chase
+    [SerializeField] GameObject player;
+
     // toggleable bool to stop enemy from moving
     [SerializeField] bool active = true;
 
     // fine tuning variables
-    private float directionFlipGrace;
+    private float directionFlipGrace = 0f;
     private float initializeTime = 1f;
     private bool onWall = false;
 
+    [SerializeField] private LayerMask groundLayer;
     Rigidbody2D body;
+    BoxCollider2D boxCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
         curMoveSpeed = moveSpeed;
+    }
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -52,13 +62,16 @@ public class EnemyScript : MonoBehaviour
         }
 
         // update movement depending on which way enemy is facing
-        if (IsFacingRight())
+        if (IsGrounded())
         {
-            body.velocity = new Vector2(curMoveSpeed, 0f);
-        }
-        else
-        {
-            body.velocity = new Vector2(-curMoveSpeed, 0f);
+            if (IsFacingRight())
+            {
+                body.velocity = new Vector2(curMoveSpeed, 0f);
+            }
+            else
+            {
+                body.velocity = new Vector2(-curMoveSpeed, 0f);
+            }
         }
 
         // update grace time between flipping direction
@@ -72,7 +85,6 @@ public class EnemyScript : MonoBehaviour
             onWall = false;
             initializeTime -= Time.deltaTime;
         }
-
     }
 
     // return true if enemy is facing right
@@ -84,12 +96,12 @@ public class EnemyScript : MonoBehaviour
     // return true if enemy is facing player
     private bool IsFacingPlayer()
     {
-        if (GameObject.Find("Player").transform.position.x > transform.position.x && IsFacingRight())
+        if (player.transform.position.x > transform.position.x && IsFacingRight())
         {
             return true;
         }
         
-        if (GameObject.Find("Player").transform.position.x < transform.position.x && !IsFacingRight())
+        if (player.transform.position.x < transform.position.x && !IsFacingRight())
         {
             return true;
         }
@@ -100,13 +112,13 @@ public class EnemyScript : MonoBehaviour
     // return true if player and enemy x positions are within .05
     private bool IsCloseEnough()
     {
-        return Math.Abs(GameObject.Find("Player").transform.position.x - transform.position.x) < .05;
+        return Math.Abs(player.transform.position.x - transform.position.x) < .05;
     }
 
     // swap the direction if about to walk off an edge
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Ground") && directionFlipGrace == 0)
+        if (/*other.CompareTag("Ground")*/(groundLayer & (1 << other.gameObject.layer)) != 0 && directionFlipGrace == 0)
         {
             transform.localScale = new Vector2(-(Mathf.Sign(body.velocity.x) * Mathf.Abs(transform.localScale.x)), transform.localScale.y);
             directionFlipGrace = 1f;
@@ -117,14 +129,25 @@ public class EnemyScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         // if hitting terrain and still trying to chase player, halt movement
-        if ((other.CompareTag("Ground") || other.CompareTag("Popcorn") || other.CompareTag("Cornstalk")) && targetsPlayer && IsFacingPlayer())
+        if ((ColliderIsGround(other) || other.CompareTag("Popcorn") || other.CompareTag("Cornstalk")) && targetsPlayer && IsFacingPlayer())
         {
             onWall = true;
         }
-        else if ((other.CompareTag("Ground") || other.CompareTag("Popcorn") || other.CompareTag("Cornstalk")) && directionFlipGrace == 0)
+        else if ((ColliderIsGround(other) || other.CompareTag("Popcorn") || other.CompareTag("Cornstalk")) && directionFlipGrace == 0)
         {
             transform.localScale = new Vector2(-(Mathf.Sign(body.velocity.x) * Mathf.Abs(transform.localScale.x)), transform.localScale.y);
             directionFlipGrace = 1f;
         }
+    }
+
+    // check if enemy is grounded
+    private bool IsGrounded(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    // checks if layer attached to collider is groundLayer
+    private bool ColliderIsGround(Collider2D other) {
+        return (groundLayer & (1 << other.gameObject.layer)) != 0;
     }
 }
