551863f9c3b0c2eb9cf9a29be61ad70147ab715a
caseyst22 <ccstrayer02@gmail.com>
2024-11-29 16:55:43 -0600

    Added leaf sprite and connected UI to collectibles scripting

--- ATG/Assets/Scripts/UIScript.cs
+++ ATG/Assets/Scripts/UIScript.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
 
-    [SerializeField] private NewBehaviourScript playerScript;
+    [SerializeField] private NewBehaviourScript movementScript;
+    [SerializeField] private PlayerTossGrowth abilityScript;
     [SerializeField] private Text healthText;
+    [SerializeField] private Text catapultText;
     [SerializeField] private Text cornText;
     [SerializeField] private Text kernelText;
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "" + playerScript.CurrentHealth;
+        healthText.text = "" + movementScript.CurrentHealth;
+        catapultText.text = "" + abilityScript.CurrentCatapulCount;
+        cornText.text = "" + abilityScript.CurrentCornCount;
+        kernelText.text = "" + abilityScript.CurrentPopcornCount;
     }
 
 }
